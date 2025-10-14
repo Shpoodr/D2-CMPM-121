474494ce8b9499e1cdfb1ec0d74f1133e8ce7e6f
Shpoodr <116927445+Shpoodr@users.noreply.github.com>
2025-10-14 22:04:02 +0000

    Step two complete. You can draw on the canvas and clear it when you want. when you arent holding the mouse button down it will not draw. Also added some organization stuff and brought everything in to a container.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
+
+let isDrawing = false;
 
 document.body.innerHTML = `
   <h1>Random Title</h1>
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <canvas id="canvas"></canvas>
+  <div class="container">
+    <div class="toolbar">
+      <button id="clear">Clear</button>
+      <canvas id="canvas"></canvas>
+    </div>
+  </div>
 `;
 
+const clear = document.getElementById("clear") as HTMLButtonElement;
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
@@ -17,4 +23,21 @@ if (ctx === null) {
   throw new Error("Failed to get 2D context");
 }
 
+canvas.addEventListener("mousedown", (event) => {
+  isDrawing = true;
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
+});
 
+canvas.addEventListener("mousemove", (event) => {
+  if (isDrawing) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+  }
+});
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+clear.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
