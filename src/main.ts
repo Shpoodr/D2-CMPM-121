b5d2b45b68416768e23ce64ad3f8767141fd6d15
Shpoodr <116927445+Shpoodr@users.noreply.github.com>
2025-10-14 21:33:11 +0000

    Step 1 completed. I added a canvas with 256 by 256 pixels with a border and backdrop. Also added a temporary title.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,19 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+  <h1>Random Title</h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <canvas id="canvas"></canvas>
 `;
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+canvas.width = 256;
+canvas.height = 256;
+
+if (ctx === null) {
+  throw new Error("Failed to get 2D context");
+}
+
+
